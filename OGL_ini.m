3ac20fdd1f6d7ac169b5c0dafd5e34993939746f
Rolando220 <roly22216@gmail.com>
2026-05-20 12:18:56 +0200

    pulizia repository

--- OGL_ini.m
+++
@@ -1,9 +0,0 @@
-% This script initializes simulation data for the OGL.mdl scheme
-%
-% Corso SGN - UNIPI - AA 2007/2008
-% Author: Lorenzo Pollini (lpollini@dsea.unipi.it)
-
-truePNG_ini;
-GUIDE_SEL = 1;
-
-T_estim = 1;
